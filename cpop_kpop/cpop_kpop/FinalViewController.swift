184bbbf3e23825e08ebb9f92313638782896ec76
Michael Chen <michael.chen@berkeley.edu>
2015-01-16 00:08:34 -0800

    connected finalViewController to storyboard

--- cpop_kpop/cpop_kpop/FinalViewController.swift
+++ cpop_kpop/cpop_kpop/FinalViewController.swift
@@ -2,22 +2,40 @@
 //  FinalViewController.swift
 //  cpop_kpop
 //
-//  Created by Seunghun Oh on 1/13/15.
+//  Created by Michael Chen on 1/15/15.
 //  Copyright (c) 2015 Michael Chen. All rights reserved.
 //
 
 import UIKit
 
 class FinalViewController: UIViewController {
-    
-    
+
     @IBOutlet var finalScore: UILabel!
     
+    let playScreen = PlayingViewController()
+    
+    finalScore.textLabel = NSString(format: "Score: %3.d", playScreen.getScore())
+    
     override func viewDidLoad() {
-        
-        let playScreen = PlayingViewController()
-        
-        finalScore.text = NSString(format: "Score: %3.d", playScreen.getScore())
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
-   
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
